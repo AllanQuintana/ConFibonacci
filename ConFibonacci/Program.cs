0a41dd4e25aa96109160afa3642a07ef44a4f45b
allan <allan@LAPTOP-VQUKN846>
2023-04-21 22:47:47 -0400

    Agregar archivos de proyecto.

---
+++ ConFibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int a, b, limite, auxiliar;
+            Console.WriteLine("Ingrese la cantidad de numeros de fibonacci");
+            int.TryParse(Console.ReadLine(), out limite);
+            a = 0;
+            b = 1;
+            
+            for (int i = 0; i < limite; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(1);
+                }
+                auxiliar = a;
+                a = b;
+                b = auxiliar + a;
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
